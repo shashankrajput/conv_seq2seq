62f4e07005f4c17d34452c0f2c224f2cb8704caf
shashank <rajput.shashank11@gmail.com>
2017-11-05 18:05:47 -0600

    First run!

--- seq2seq/contrib/seq2seq/helper.py
+++ seq2seq/contrib/seq2seq/helper.py
@@ -32,8 +32,8 @@ import abc
 
 import six
 
-from tensorflow.contrib.distributions.python.ops import bernoulli
-from tensorflow.contrib.distributions.python.ops import categorical
+from tensorflow.python.ops.distributions import bernoulli
+from tensorflow.python.ops.distributions import categorical
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.layers import base as layers_base
--- train_conv_seq2seq.sh
+++ train_conv_seq2seq.sh
@@ -1,23 +1,19 @@
 export PYTHONIOENCODING=UTF-8
 
-export DATA_PATH=/home/xutan/nmt/seq2seq/data/iwslt14.tokenized.de-en
+export VOCAB_SOURCE=${HOME}/nmt_data/toy_reverse/train/vocab.sources.txt
+export VOCAB_TARGET=${HOME}/nmt_data/toy_reverse/train/vocab.targets.txt
+export TRAIN_SOURCES=${HOME}/nmt_data/toy_reverse/train/sources.txt
+export TRAIN_TARGETS=${HOME}/nmt_data/toy_reverse/train/targets.txt
+export DEV_SOURCES=${HOME}/nmt_data/toy_reverse/dev/sources.txt
+export DEV_TARGETS=${HOME}/nmt_data/toy_reverse/dev/targets.txt
 
-export VOCAB_SOURCE=${DATA_PATH}/vocab.de
-export VOCAB_TARGET=${DATA_PATH}/vocab.en
-export TRAIN_SOURCES=${DATA_PATH}/train.de
-export TRAIN_TARGETS=${DATA_PATH}/train.en
-export DEV_SOURCES=${DATA_PATH}/valid.de
-export DEV_TARGETS=${DATA_PATH}/valid.en
-export TEST_SOURCES=${DATA_PATH}/test.de
-export TEST_TARGETS=${DATA_PATH}/test.en
-
-export TRAIN_STEPS=1000000
-
+export DEV_TARGETS_REF=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+export TRAIN_STEPS=1000
 
 export MODEL_DIR=${TMPDIR:-/tmp}/nmt_conv_seq2seq
 mkdir -p $MODEL_DIR
 
-'''
+
 python -m bin.train \
   --config_paths="
       ./example_configs/conv_seq2seq.yml,
@@ -41,16 +37,16 @@ python -m bin.train \
        target_files:
         - $DEV_TARGETS" \
   --batch_size 32 \
-  --eval_every_n_steps 5000 \
+  --eval_every_n_steps 50 \
   --train_steps $TRAIN_STEPS \
   --output_dir $MODEL_DIR
 
+
+
 '''
-
-
 export PRED_DIR=${MODEL_DIR}/pred
 mkdir -p ${PRED_DIR}
-'''
+
 ###with greedy search
 python -m bin.infer \
   --tasks "
@@ -65,6 +61,8 @@ python -m bin.infer \
       source_files:
         - $TEST_SOURCES" \
   > ${PRED_DIR}/predictions.txt
+
+'''
 
 '''
 ###with beam search
@@ -87,6 +85,7 @@ python -m bin.infer \
 
 
 ./bin/tools/multi-bleu.perl ${TEST_TARGETS} < ${PRED_DIR}/predictions.txt
+'''
 
 
 
