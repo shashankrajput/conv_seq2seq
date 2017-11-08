04b34cd07402109eada143283ff3208c93b195f2
Shashank Rajput <rajput.shashank11@gmail.com>
2017-11-07 18:37:51 -0600

    New config for probabilistic encoder.

---
+++ train_conv_seq2seq_prob.sh
@@ -0,0 +1,91 @@
+export PYTHONIOENCODING=UTF-8
+
+export VOCAB_SOURCE=${HOME}/nmt_data/toy_reverse/train/vocab.sources.txt
+export VOCAB_TARGET=${HOME}/nmt_data/toy_reverse/train/vocab.targets.txt
+export TRAIN_SOURCES=${HOME}/nmt_data/toy_reverse/train/sources.txt
+export TRAIN_TARGETS=${HOME}/nmt_data/toy_reverse/train/targets.txt
+export DEV_SOURCES=${HOME}/nmt_data/toy_reverse/dev/sources.txt
+export DEV_TARGETS=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+
+export DEV_TARGETS_REF=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+export TRAIN_STEPS=1000
+
+export MODEL_DIR=${TMPDIR:-/tmp}/nmt_conv_seq2seq_prob
+mkdir -p $MODEL_DIR
+
+
+python -m bin.train \
+  --config_paths="
+      ./example_configs/conv_prob_seq2seq.yml,
+      ./example_configs/train_seq2seq.yml,
+      ./example_configs/text_metrics_bpe.yml" \
+  --model_params "
+      vocab_source: $VOCAB_SOURCE
+      vocab_target: $VOCAB_TARGET" \
+  --input_pipeline_train "
+    class: ParallelTextInputPipelineFairseq
+    params:
+      source_files:
+        - $TRAIN_SOURCES
+      target_files:
+        - $TRAIN_TARGETS" \
+  --input_pipeline_dev "
+    class: ParallelTextInputPipelineFairseq
+    params:
+       source_files:
+        - $DEV_SOURCES
+       target_files:
+        - $DEV_TARGETS" \
+  --batch_size 32 \
+  --eval_every_n_steps 50 \
+  --train_steps $TRAIN_STEPS \
+  --output_dir $MODEL_DIR
+
+
+
+'''
+export PRED_DIR=${MODEL_DIR}/pred
+mkdir -p ${PRED_DIR}
+
+###with greedy search
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText" \
+  --model_dir $MODEL_DIR \
+  --model_params "
+    inference.beam_search.beam_width: 1 
+    decoder.class: seq2seq.decoders.ConvDecoderFairseq" \
+  --input_pipeline "
+    class: ParallelTextInputPipelineFairseq
+    params:
+      source_files:
+        - $TEST_SOURCES" \
+  > ${PRED_DIR}/predictions.txt
+
+'''
+
+'''
+###with beam search
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText
+    - class: DumpBeams
+      params:
+        file: ${PRED_DIR}/beams.npz" \
+  --model_dir $MODEL_DIR \
+  --model_params "
+    inference.beam_search.beam_width: 5 
+    decoder.class: seq2seq.decoders.ConvDecoderFairseqBS" \
+  --input_pipeline "
+    class: ParallelTextInputPipelineFairseq
+    params:
+      source_files:
+        - $TEST_SOURCES" \
+  > ${PRED_DIR}/predictions.txt
+
+
+./bin/tools/multi-bleu.perl ${TEST_TARGETS} < ${PRED_DIR}/predictions.txt
+'''
+
+
+
